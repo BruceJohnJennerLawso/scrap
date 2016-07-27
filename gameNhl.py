357c4297495c63ad495fc25cd440388d076aa4a8
bruce <johnnybgoode@rogers.com>
2016-07-26 22:41:08 -0400

    restructured both nhl and watMu teams to old style structure without breaking anything yet

--- gameNhl.py
+++ gameNhl.py
@@ -8,77 +8,77 @@ from game import *
 
 
 class nhlGame(Game):
-	def __init__(self, dateString, Location, homeTeamName, homeTeamGoalsScored, awayTeamName, awayTeamGoalsScored, extraTimeString):
+	def __init__(self, dateString, Location, gameResult, goalsFor, goalsAgainst, opponentName, thisTeamName, gameEndedIn):
 		super(nhlGame, self).__init__()
-		self.addLayer([dateString, Location, homeTeamName, homeTeamGoalsScored, awayTeamName, awayTeamGoalsScored, extraTimeString])
+		
+		if(Location == '@'):
+			gameLocation = '@%s_%s' % (opponentName, dateString)
+		elif(Location == 'H'):
+			gameLocation = '@%s_%s' % (thisTeamName, dateString)
+		## gonna use this to track back to what barn the game
+		## would have been played in
+		
+		self.addLayer([dateString, gameLocation, gameResult, goalsFor, goalsAgainst, thisTeamName, opponentName, gameEndedIn])
 		self.layerCount += 1
-		## first layer is the absolute bare minimum, just who played, who was
-		## the home team, and how many goals each team scored, along with
-		## whether the game went to OT or shootout. We should have
-		## access to this data for any game we are looking at
-		
-		## if we have access to more data, like shots, teamwise PIM, PPG, SHG,
-		## attendance, time of game (if its even worth handling that)
 	
-	def getHomeTeam(self):
-		return self.Layers[0][2]
-		
-	def getAwayTeam(self):
-		return self.Layers[0][4]
+	
 
-	def getGoalsFor(self, teamName):
-		if(teamName == self.getHomeTeam()):
-			return self.Layers[0][3]
-		elif(teamName == self.getAwayTeam())
-			return self.Layers[0][5]
-		## should hopefully already be in 
+	def getthisTeamName(self):
+		return self.Layers[0][5]
+	
+	def Won(self):
+		if(self.Layers[0][2] == 'W'):
+			return True
+		else:
+			return False
+			
+	def Tied(self):
+		if(self.Layers[0][2] == 'T'):
+			return True
+		else:
+			return False	
+			
+	def Lost(self):
+		if(self.Layers[0][2] == 'L'):
+			return True
+		else:
+			return False	
+
+	def getExtraTimeString(self):
+		return self.Layers[0][7]
 		
-	def getGoalsAgainst(self, teamName):
-		if(teamName == self.getHomeTeam()):
-			return self.Layers[0][5]
-		elif(teamName == self.getAwayTeam())
-			return self.Layers[0][3]
-		## should hopefully already be in 
+	def decidedInOvertime(self):
+		if(self.Layers[0][7] == 'OT'):
+			return True
+		else:
+			return False
 		
-	def getGoalDifferential(self):
-		diff = self.getGoalsFor()-self.getGoalsAgainst()
-		return diff		
+	def decidedInShootout(self):
+		if(self.Layers[0][7] == 'SO'):
+			return True
+		else:
+			return False
+			
+	def decidedInExtraTime(self):
+		if((self.decidedInOvertime())or(self.decidedInShootout())):
+			return True
+		else:
+			return False
+
 		
 	def getPointsEarned(self):
 		if(self.Won()):
 			return 2
 		elif(self.Tied()):
 			return 1
+		elif((self.Lost())and(self.decidedInExtraTime())):
+			return 1
 		else:
 			return 0
-			## regardless of whether we lost or the game hasnt been played yet,
-			## no points are earned
+			## regardless of whether we lost in regulation or the game hasnt
+			## been played yet, no points are earned
 			
-	
-	def Won(self, teamName):
-		if(self.getGoalsFor(teamName)>self.getGoalsAgainst(teamName)):
-			return True
-		else:
-			return False
-			
-	def Tied(self):
-		if(self.getGoalsFor(teamName)==self.getGoalsAgainst(teamName)):
-			return True
-		else:
-			return False	
-			
-	def Lost(self):
-		if(self.getGoalsFor(teamName)<self.getGoalsAgainst(teamName)):
-			return True
-		else:
-			return False						
+					
 
-	def getExtraTimeString(self):
-		return self.Layers[0][6]
 
-	def getGameClosenessIndex(self):
-		if(self.Tied() != True):
-			return 1/float(abs(self.getGoalDifferential()))
-		else:
-			return 1
 
