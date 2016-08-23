a2e1f33b7728e2b303fe54f80db35d0f20408c97
bruce <johnnybgoode@rogers.com>
2016-08-23 19:41:45 -0400

    reworked the teamSummary to actually be a team summary, and moved the old version of that to a new playerData file

---
+++ playerData.py
@@ -0,0 +1,44 @@
+## playerData.py ###############################################################
+################################################################################
+
+from sys import argv
+from outputs import *
+
+from collections import Counter
+
+
+if(__name__ == "__main__"):
+		
+	leagueId = argv[1]
+	## ie 'watMu'
+	levelId = argv[2]
+	## ie 'beginner'
+	seasonId = argv[3]
+	## ie 'fall2015'
+	teamName = argv[4]
+	## ie 'The Mighty Dads'
+	
+	## ids needed to open the proper folders and csv files contained within
+	seasons = getAllSeasons(leagueId, levelId)
+	## retrieve list of seasons from the manifest for this level
+	##franchises = getFranchiseList(leagueId, levelId)
+	franchises = False
+	
+	seasonIndexList = getSeasonIndexList(leagueId)
+	for season in seasons:
+		if(season.getSeasonId() == seasonId):
+			for team in season.Teams:
+				if(team.getTeamName() == teamName):
+					
+					print team.getDescriptionString()
+					
+					teamExpectedMawquee = 0.000
+					
+					for player in team.getPlayers():
+						player.getStatsLine(getSeasonIndexById(seasonId, getSeasonIndexList(leagueId)))
+						print getSeasonIndexById(seasonId, getSeasonIndexList(leagueId))
+						teamExpectedMawquee += player.getPlayerMaAWQI(getSeasonIndexById(seasonId, getSeasonIndexList(leagueId)))
+					
+					teamExpectedMawquee /= float(len(team.getPlayers()))
+					print "Team expected MaAWQI %.3f, Actual %.3f, diff %.3f" % (teamExpectedMawquee, team.getMaAWQI(), (team.getMaAWQI()-teamExpectedMawquee))
+					
--- teamSummary.py
+++ teamSummary.py
@@ -6,7 +6,6 @@
 from sys import argv
 from outputs import *
 
-from collections import Counter
 
 
 if(__name__ == "__main__"):
@@ -15,34 +14,19 @@ if(__name__ == "__main__"):
 	## ie 'watMu'
 	levelId = argv[2]
 	## ie 'beginner'
-	seasonId = argv[3]
-	## ie 'fall2015'
-	teamName = argv[4]
-	## ie 'The Mighty Dads'
 	
 	## ids needed to open the proper folders and csv files contained within
 	seasons = getAllSeasons(leagueId, levelId)
 	## retrieve list of seasons from the manifest for this level
 	##franchises = getFranchiseList(leagueId, levelId)
 	franchises = False
-	
+
 	seasonIndexList = getSeasonIndexList(leagueId)
-	for season in seasons:
-		if(season.getSeasonId() == seasonId):
-			for team in season.Teams:
-				print team.getDescriptionString()
-				
-				if(team.getTeamName() == teamName):
-					print team.getDescriptionString()
-					
-					teamExpectedMawquee = 0.000
-					
-					for player in team.getPlayers():
-						player.getStatsLine(getSeasonIndexById(seasonId, getSeasonIndexList(leagueId)))
-						print getSeasonIndexById(seasonId, getSeasonIndexList(leagueId))
-						teamExpectedMawquee += player.getPlayerMaAWQI(getSeasonIndexById(seasonId, getSeasonIndexList(leagueId)))
-					teamExpectedMawquee /= float(len(team.getPlayers()))
-					print "Team expected MaAWQI %.3f, Actual %.3f, diff %.3f" % (teamExpectedMawquee, team.getMaAWQI(), (team.getMaAWQI()-teamExpectedMawquee))
 	
-	teamExpectedMawquees = []
-	playoffPercentages = []
+	for season in seasons:
+		print "################################################################################"
+		print season.seasonId, "###########################################################################"
+		print "################################################################################\n"		
+		
+		for team in season.Teams:
+			print team.getDescriptionString(), "\n"
