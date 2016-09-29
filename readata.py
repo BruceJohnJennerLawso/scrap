0564b77713884689b13fcd96a27d5d44c5dae435
bruce <johnnybgoode@rogers.com>
2016-09-28 23:16:33 -0400

    made everything in readata multithreaded

--- readata.py
+++ readata.py
@@ -137,78 +137,8 @@ def graphPlayoffGoalDifferentialRelations(seasons, leagueId, levelId):
 
 	return 0
 
-
-if(__name__ == "__main__"):
-	
-	leagueId = argv[1]
-	## ie 'watMu'
-	levelId = argv[2]
-	## ie 'beginner'
-	
-	num_cores = int(mp.cpu_count()*0.5)
-	
-	print "Running readata with argv ", argv, '\non %i cores, %i available' % (mp.cpu_count(), num_cores)
-	
-	## ids needed to open the proper folders and csv files contained within
-	seasons = getAllSeasons(leagueId, levelId)
-	## retrieve list of seasons from the manifest for this level
-	##franchises = getFranchiseList(leagueId, levelId)
-	
-	
-	
-	
-	p1 = mp.Process(target = graphPlayoffSuccessRelations, args=(seasons, leagueId, levelId), name = "Playoff Success")
-	p2 = mp.Process(target = graphPlayoffOffenceRelations, args=(seasons, leagueId, levelId), name = "Playoff Offence")
-	p3 = mp.Process(target = graphPlayoffDefenceRelations, args=(seasons, leagueId, levelId), name = "Playoff Defence")
-	p4 = mp.Process(target = graphPlayoffGoalDifferentialRelations, args=(seasons, leagueId, levelId), name = "Playoff Goal Differential")
-
-	procs = [p1, p2, p3, p4]
-	activeProcs = []
-	
-	while(True):
-		time.sleep(1)
-		
-		runningProcesses = 0
-		for p in activeProcs:
-			if((p.exitcode != 0)and(p.is_alive())):
-				runningProcesses += 1
-
-		finishedProcesses = 0
-		for p in procs:
-			if(p.exitcode == 0):
-				finishedProcesses += 1
-		if(finishedProcesses == len(procs)):
-			break
-		
-		print "%i running processes, %i finished %i cores available\n" % (runningProcesses, finishedProcesses, num_cores)
-		print "Total Process List\n", [[p.name, p.exitcode, p.is_alive(), p.pid] for p in procs], '\n'
-		print "Active Process List\n", [[p.name, p.exitcode, p.is_alive(), p.pid] for p in activeProcs], '\n\n'
-		sys.stdout.write("\x1b[2J\x1b[H");
-		if(runningProcesses < num_cores):
-			for p in procs:
-				if p not in activeProcs:
-					activeProcs.append(p)
-					p.start()
-					break
-
-		
-		
-	##graphPlayoffSuccessRelations(seasons, leagueId, levelId)
-	##graphPlayoffOffenceRelations(seasons, leagueId, levelId)
-	##graphPlayoffDefenceRelations(seasons, leagueId, levelId)
-
-
-
-
-
-
-
-
-
-
-
-
-	graphTeamsHistogram(leagueId, levelId, True,\
+def plotHistograms(seasons, leagueId, levelId):
+		graphTeamsHistogram(leagueId, levelId, True,\
 	getStatContainer(Team.getPlayoffSuccessRating, 'playoffSuccess', 'Playoff Success', seasons, leagueId, levelId),\
 	getStatContainer(Team.getPlayoffAverageGoalDifferential, 'playoffAvgPlusMinus', 'Playoff Average Goal Differential', seasons, leagueId, levelId),\
 	getStatContainer(Team.getPlayoffGoalsAgainstAverage, 'playoffOffence', 'Playoff Goals For Average', seasons, leagueId, levelId),\
@@ -239,7 +169,10 @@ if(__name__ == "__main__"):
 	getStatContainer(Team.getMaAWQI, 'MaAWQI', 'Mean Adjusted Average Win Quality Index', seasons, leagueId, levelId),\
 	getStatContainer(Team.getSeasonPlusMinus, 'PlusMinus', 'Season Goal Differential (+/-)', seasons, leagueId, levelId),\
 	getStatContainer(Team.getPointsPercentage, 'PtsPct', 'Points Percentage', seasons, leagueId, levelId))				 	
+	
+	return 0
 
+def plotHeatmap(seasons, leagueId, levelId):
 	plotVariablesHeatmap(leagueId, levelId, True,\
 	getStatContainer(Team.getPlayoffAverageGoalDifferential, 'playoffAvgPlusMinus', 'Playoff Average Goal Differential', seasons, leagueId, levelId),\
 	getStatContainer(Team.getPlayoffSuccessRating, 'playoffSuccess', 'Playoff Success', seasons, leagueId, levelId),\
@@ -255,4 +188,70 @@ if(__name__ == "__main__"):
 	getStatContainer(Team.getMaAWQI, 'MaAWQI', 'Mean Adjusted Average Win Quality Index', seasons, leagueId, levelId),\
 	getStatContainer(Team.getSeasonPlusMinus, 'PlusMinus', 'Season Goal Differential (+/-)', seasons, leagueId, levelId),\
 	getStatContainer(Team.getPointsPercentage, 'PtsPct', 'Points Percentage', seasons, leagueId, levelId))				 	
+
+	return 0
+
+
+if(__name__ == "__main__"):
+	
+	leagueId = argv[1]
+	## ie 'watMu'
+	levelId = argv[2]
+	## ie 'beginner'
+	
+	num_cores = int(mp.cpu_count()*0.5)
+	
+	print "Running readata with argv ", argv, '\non %i cores, %i available' % (mp.cpu_count(), num_cores)
+	
+	## ids needed to open the proper folders and csv files contained within
+	seasons = getAllSeasons(leagueId, levelId)
+	## retrieve list of seasons from the manifest for this level
+	##franchises = getFranchiseList(leagueId, levelId)
+	
+	
+	
+	
+	p1 = mp.Process(target = graphPlayoffSuccessRelations, args=(seasons, leagueId, levelId), name = "Playoff Success")
+	p2 = mp.Process(target = graphPlayoffOffenceRelations, args=(seasons, leagueId, levelId), name = "Playoff Offence")
+	p3 = mp.Process(target = graphPlayoffDefenceRelations, args=(seasons, leagueId, levelId), name = "Playoff Defence")
+	p4 = mp.Process(target = graphPlayoffGoalDifferentialRelations, args=(seasons, leagueId, levelId), name = "Playoff Goal Differential")
+	p5 = mp.Process(target = plotHistograms, args=(seasons, leagueId, levelId), name = "Histograms")
+	p6 = mp.Process(target = plotHeatmap, args=(seasons, leagueId, levelId), name = "Heatmap")		
+
+	procs = [p1, p2, p3, p4, p5, p6]
+	activeProcs = []
+	
+	while(True):
+		time.sleep(1)
+		
+		runningProcesses = 0
+		for p in activeProcs:
+			if((p.exitcode != 0)and(p.is_alive())):
+				runningProcesses += 1
+
+		finishedProcesses = 0
+		for p in procs:
+			if(p.exitcode == 0):
+				finishedProcesses += 1
+		if(finishedProcesses == len(procs)):
+			break
+		
+		print "%i running processes, %i finished %i cores available\n" % (runningProcesses, finishedProcesses, num_cores)
+		print "Total Process List\n", [[p.name, p.exitcode, p.is_alive(), p.pid] for p in procs], '\n'
+		print "Active Process List\n", [[p.name, p.exitcode, p.is_alive(), p.pid] for p in activeProcs], '\n\n'
+		sys.stdout.write("\x1b[2J\x1b[H");
+		if(runningProcesses < num_cores):
+			for p in procs:
+				if p not in activeProcs:
+					activeProcs.append(p)
+					p.start()
+					break
+
+
+
+
+
+
+
+
 
