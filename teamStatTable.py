bd2c05e7079db8d1a2c1b5d8885f632f4f16d6b1
bruce <johnnybgoode@rogers.com>
2016-10-22 23:17:33 -0400

    added a rough script to generate simple stat tables that I can copy paste

---
+++ teamStatTable.py
@@ -0,0 +1,31 @@
+## teamStatTable.py ############################################################
+## Get a simple list with the year and the value of a ##########################
+## given stat for that year in the terminal ####################################
+################################################################################
+
+
+from sys import argv
+from outputs import *
+
+
+
+
+if(__name__ == "__main__"):
+	leagueId = argv[1]
+	## ie 'watMu'
+	levelId = argv[2]
+	## ie 'beginner'
+	teamName = argv[3]
+	
+	## ids needed to open the proper folders and csv files contained within
+	seasons = getAllSeasons(leagueId, levelId)
+	## retrieve list of seasons from the manifest for this level
+	##franchises = getFranchiseList(leagueId, levelId)
+	franchises = False
+
+	seasonIndexList = getSeasonIndexList(leagueId)
+
+	for season in seasons:
+		for team in season.Teams:
+			if(team.getTeamName() == teamName):
+				print "%i,%.3f" % (int(season.seasonId),team.getSeasonPointsTotal())
