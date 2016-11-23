33b77a8a044bf803aebaff77cbe1673edc45434e
bruce <johnnybgoode@rogers.com>
2016-11-23 18:25:54 -0500

    created the new module for parts of seasons

---
+++ seasonParts.py
@@ -0,0 +1,4 @@
+## seasonParts.py ##############################################################
+## object of type seasonPart which represents a list of games in the parent ####
+## season, and the stats that can be associated with those games ###############
+################################################################################
