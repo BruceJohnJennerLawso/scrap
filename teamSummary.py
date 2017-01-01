e67b53d8d3ea0c8c8188138864148e782ab576c0
bruce <johnnybgoode@rogers.com>
2016-12-31 23:31:17 -0500

    updated the teamSummary so it still gives complete game summaries for the season when an individual year and an individual season is selected, but not when a spread of games is being looked at (ie  assessing a team over a time period)
    Next steps are to start ripping out the old crap that used to
    form the core of the stat counting of teams, so that season parts
    is the only way of accessing data

--- teamSummary.py
+++ teamSummary.py
@@ -13,7 +13,11 @@ if(__name__ == "__main__"):
 	levelId = argv[2]
 	## ie 'beginner'
 	
-	
+	try:
+		with open('./data/%s/%s/seasons.csv' % (leagueId, levelId), 'rb') as foo:
+			outputType = "generic"
+	except IOError:		
+		outputType = "detailed"	
 	teamNames = 0
 	if(len(argv) > 3):
 		teamNames = argv[3:len(argv)]
@@ -41,10 +45,12 @@ if(__name__ == "__main__"):
 				for team in season.Teams:
 					if(team.getTeamName() == teamName):
 						print team.getDescriptionString(), "\n"			
-						i = 1
-						print team.getSeasonGames()
-						for gm in team.getSeasonGames():
-							##print "game no %i," % i, gm.Layers[0], "\nOQI %.3f, DQI %.3f, DiffQI %.3f, True Diff %.3f, DiffQualOverDiff %.3f" % (gm.offenceQualityIndex, gm.defenceQualityIndex, gm.diffQualityIndex, gm.getGoalDifferential(), gm.getDiffQualMargin())
-							print gm.getGameDescription()
-							i += 1
+						
+						if(outputType == "detailed"):
+							i = 1
+							print team.getSeasonGames()
+							for gm in team.getSeasonGames():
+								##print "game no %i," % i, gm.Layers[0], "\nOQI %.3f, DQI %.3f, DiffQI %.3f, True Diff %.3f, DiffQualOverDiff %.3f" % (gm.offenceQualityIndex, gm.defenceQualityIndex, gm.diffQualityIndex, gm.getGoalDifferential(), gm.getDiffQualMargin())
+								print gm.getGameDescription()
+								i += 1
 						
