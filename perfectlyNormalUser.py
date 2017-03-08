f9cddd87e3a55c38551b93de33183014ecc83650
bruce <johnnybgoode@rogers.com>
2017-03-08 12:27:46 -0500

    moving the mimicker to its own module for convenience

---
+++ perfectlyNormalUser.py
@@ -0,0 +1,4 @@
+## perfectlyNormalUser.py ######################################################
+## nothing to see here human, just another completely human ####################
+## visitor to the website of which you are running, you filthy meatbag #########
+################################################################################
