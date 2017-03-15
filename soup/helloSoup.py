a1dbb1251c542e09adc5b6b5898c575a93e60da6
bruce <johnnybgoode@rogers.com>
2017-03-15 11:11:24 -0400

    getting much closer with this to having a proper table grabber

--- soup/helloSoup.py
+++ soup/helloSoup.py
@@ -13,8 +13,22 @@ if(__name__ == "__main__"):
 
 	page = requests.get(url)
 	
-	soup = BeautifulSoup(page.content)
-	##table = soup.find(lambda tag: tag.name=='table' and tag.has_key('id') and tag['id']=="Table1") 
-	##rows = table.findAll(lambda tag: tag.name=='tr')
-	for link in soup.find_all('a'):
-		print(link.get('href'))
+	soup = BeautifulSoup(page.content, "lxml")
+	##table = soup.find(lambda tag: tag.name=='table' and tag.has_key('class') and tag['class']=="refpays") 
+	##table = table.find(lambda tag: tag.name=='tbody') 	
+	table = soup.find(lambda tag: tag.name=='table', class_ ="refpays") 
+	rows = table.findAll(lambda tag: tag.name=='tr')
+	
+	
+	for row in rows:
+		print row
+	print "\n\nFlattening to csv\n"
+	outputRows = []
+	for row in rows:
+		print row, row.find_all('tr')
+		outputRows.append([val.text.encode('utf8') for val in row.find_all('td') + row.find_all('th')])		
+
+	for row in outputRows:
+		print row, len(row)	
+	##for link in soup.find_all('a'):
+	##	print(link.get('href'))
