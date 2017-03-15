0b0e59d210afd4324ee6fb88b3d84376a8ef0fc1
bruce <johnnybgoode@rogers.com>
2017-03-14 22:03:40 -0400

    got the soup example back to workability with lxml

--- soup/helloSoup.py
+++ soup/helloSoup.py
@@ -2,3 +2,19 @@
 ## just mucking around with beautiful soup to get ##############################
 ## a feel for it ###############################################################
 ################################################################################
+from bs4 import BeautifulSoup
+from lxml import html
+import requests
+
+
+if(__name__ == "__main__"):
+
+	url = "https://strobe.uwaterloo.ca/athletics/intramurals/teams.php?team=11256"
+
+	page = requests.get(url)
+	
+	soup = BeautifulSoup(page.content)
+	##table = soup.find(lambda tag: tag.name=='table' and tag.has_key('id') and tag['id']=="Table1") 
+	##rows = table.findAll(lambda tag: tag.name=='tr')
+	for link in soup.find_all('a'):
+		print(link.get('href'))
