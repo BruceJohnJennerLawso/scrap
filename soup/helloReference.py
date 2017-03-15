9fc84bce46d8b90d4b492bdc033285bcf20007cd
bruce <johnnybgoode@rogers.com>
2017-03-15 13:45:29 -0400

    trying the generalized table scraper on watMu right now

--- soup/helloReference.py
+++ soup/helloReference.py
@@ -7,17 +7,11 @@ from lxml import html
 import requests
 
 
-if(__name__ == "__main__"):
-
-	url = "https://strobe.uwaterloo.ca/athletics/intramurals/teams.php?team=11256"
-	url = "http://www.hockey-reference.com/teams/TOR/2017_games.html"
-	##url = "http://www.hockey-reference.com/teams/MTW/1918_games.html"
+def getTableInRows(url, tableTarget, debugInfo=False):
 	page = requests.get(url)
-	
 	soup = BeautifulSoup(page.content, "lxml")
-	##table = soup.find(lambda tag: tag.name=='table' and tag.has_key('class') and tag['class']=="refpays") 
-	##table = table.find(lambda tag: tag.name=='tbody') 	
-	table = soup.find(lambda tag: tag.name=='table', {"id": "games"}) 
+	##{"id": tableId}
+	table = soup.find(lambda tag: tag.name=='table', tableTarget) 
 	rows = table.findAll(lambda tag: tag.name=='tr')
 	
 	
@@ -31,7 +25,16 @@ if(__name__ == "__main__"):
 	for row in rows:
 		##print row, row.find_all('tr')
 		headerRows.append([val.text.encode('utf8') for val in row.find_all('th')])	
-		outputRows.append([val.text.encode('utf8') for val in row.find_all('th')+row.find_all('td')])		
+		##outputRows.append([val.text.encode('utf8') for val in row.find_all('th')+row.find_all('td')])
+		outputRows.append([val.text.encode('utf8') for val in row.find_all('td', 'th')])		
+		## important note here, some evil
+		## tables such as hockey reference like to make data in the first column
+		## (ie game number) a <th> element, like its another header but vertical
+		## because they hate me
+	for row in outputRows:
+		print row
+
+				
 	tableWidth = max([len(row) for row in headerRows])
 	## this is to single out that blasted "meta header" row thats often present
 	## but not always in a teams hockey reference page
@@ -39,6 +42,7 @@ if(__name__ == "__main__"):
 	outputRows = [row for row in outputRows if row not in headerRows]
 	headerRows = [row for row in headerRows if (len(row)==tableWidth)]
 	headerRows = map(list, set(map(tuple,headerRows)))
+	## remove duplicate header rows (in list form) from the list of lists
 	
 	print "## Header Rows ##"
 	for row in headerRows:
@@ -51,5 +55,13 @@ if(__name__ == "__main__"):
 		if(len(row) != tableWidth):
 			print "Bad row does not match widths of the others"
 		i+=1	
-	##for link in soup.find_all('a'):
-	##	print(link.get('href'))
+	return (headerRows, outputRows)
+
+if(__name__ == "__main__"):
+
+	url = "https://strobe.uwaterloo.ca/athletics/intramurals/teams.php?team=11256"
+	##url = "http://www.hockey-reference.com/teams/TOR/2017_games.html"
+	##url = "http://www.hockey-reference.com/teams/MTW/1918_games.html"
+	##page = requests.get(url)
+	getTableInRows(url, {"class", "refpays"})
+	
