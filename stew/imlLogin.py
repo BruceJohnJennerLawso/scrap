15f96b455d04d51b3f35218141d784c6205f041c
bruce <johnnybgoode@rogers.com>
2017-04-24 16:01:12 -0400

    got the credentials filled into the new form, just having trouble clicking the login button

--- stew/imlLogin.py
+++ stew/imlLogin.py
@@ -36,37 +36,24 @@ def getAuthenticatedBrowser(username, password):
 	url = "https://www.imleagues.com/spa/team/662222b25eb54b88b4eebb4090cb455a/home"
 	with closing(Firefox()) as browser:	
 
-		browser.get("https://nike.uwaterloo.ca/Login.aspx?soi=IM&ref=Intramurals")
-		element = browser.find_element_by_id("ctl00_contentMain_ASPxRoundPanel2_loginMain_UserName_I")
+		browser.get("https://warrior.uwaterloo.ca/Account/Login?returnUrl=%2F")
+		element = browser.find_element_by_name("UserName")
 		element.send_keys(username)
-		element = browser.find_element_by_id("ctl00_contentMain_ASPxRoundPanel2_loginMain_Password_I")
+		element = browser.find_element_by_name("Password")
 		element.send_keys(password)	
 		
-		buttonId = 'ctl00_contentMain_ASPxRoundPanel2_loginMain_LoginButton'		
+		buttonId = 'Log In'		
 		WebDriverWait(browser, timeout=10).until(
-				lambda x: x.find_element_by_id(buttonId))
+				lambda x: x.find_element_by_value(buttonId))
 		button = browser.find_element_by_id(buttonId)
 		button.click()
-		while(browser.current_url != "https://www.imleagues.com/spa/member/player"):
+		while(browser.current_url != "https://warrior.uwaterloo.ca/"):
 			pass
 		print "Finished signing into IMLeagues"
-		semesters = [semester]
-		for semester in semesters:
-			
-			with open('%shooks.csv' % semester, 'rb') as f:
-				reader = csv.reader(f)
-				i = 0
-				for row in reader:
-					i+=1
-					print row
-					teamId = row[0]
-					sportId = int(row[1])
-					levelId = row[2]
-					saveIdsInLinkedLeague(browser, "https://www.imleagues.com/spa/team/%s/home" % teamId, sportId, levelId, semester)
 
 
 if(__name__ == "__main__"):
-	if(len(argv) >= 4):
+	if(len(argv) >= 2):
 		username = argv[1]
 		password = argv[2]
 		##semester = argv[3]
@@ -74,3 +61,4 @@ if(__name__ == "__main__"):
 		username = getTextInput("Please Input Username: ")
 		password = getTextInput("Please Input Password: ")
 		##semester = getTextInput("Please Semester to build teamIds for: ")			
+	getAuthenticatedBrowser(username, password)
